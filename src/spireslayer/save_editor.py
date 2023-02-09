9ff67e6a411c1aab60915ecf3bd0e3779f4ae9f5
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-02-09 16:06:04 +0700

    Add SaveEditor test

--- src/spireslayer/save_editor.py
+++ src/spireslayer/save_editor.py
@@ -73,19 +73,15 @@ class SaveEditor(object):
 
     def update_current_health(self, health: int = 500):
         self.json_save_data['current_health'] = health
-        assert self.get_json().get('current_health') == health
 
     def update_max_health(self, health: int = 500):
         self.json_save_data['max_health'] = health
-        assert self.get_json().get('max_health') == health
 
     def update_max_orbs(self, max_orbs: int = 10):
         self.json_save_data['max_orbs'] = max_orbs
-        assert self.get_json().get('max_orbs') == max_orbs
 
     def update_hand_size(self, hand_size: int = 10):
         self.json_save_data['hand_size'] = hand_size
-        assert self.get_json().get('hand_size') == hand_size
 
     def update_energy_per_turn(self, red: int = 20):
         self.json_save_data['red'] = red
