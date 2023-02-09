cb4b904a040572ebcf1119036fde46fc93a19998
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-02-09 11:08:56 +0700

    Move deck example from decks.py to template.py

--- src/spireslayer/decks.py
+++ src/spireslayer/decks.py
@@ -19,33 +19,3 @@ class Deck(object):
         return [
             card.to_json() for card in self.card_list
         ]
-
-
-defect_lightning_deck = Deck([
-    Card(Card.LEAP),
-    Card(Card.LEAP),
-    Card(Card.LEAP),
-    Card(Card.ZAP),
-    Card(Card.ZAP),
-    Card(Card.ZAP),
-    Card(Card.ZAP),
-    Card(Card.DEFRAGMENT),
-    Card(Card.DEFRAGMENT),
-    Card(Card.DEFRAGMENT),
-    Card(Card.DEFRAGMENT),
-    Card(Card.THUNDER_STRIKE),
-])
-
-defect_frost_deck = Deck([
-    Card(Card.GLACIER),
-    Card(Card.GLACIER),
-    Card(Card.GLACIER),
-    Card(Card.DEFRAGMENT),
-    Card(Card.DEFRAGMENT),
-    Card(Card.DEFRAGMENT),
-    Card(Card.BLIZZARD),
-    Card(Card.BLIZZARD),
-    Card(Card.BLIZZARD),
-    Card(Card.BLIZZARD),
-    Card(Card.BLIZZARD),
-])
---
+++ src/spireslayer/template.py
@@ -0,0 +1,31 @@
+from .card import Card
+from .decks import Deck
+
+defect_lightning_deck = Deck([
+    Card(Card.LEAP),
+    Card(Card.LEAP),
+    Card(Card.LEAP),
+    Card(Card.ZAP),
+    Card(Card.ZAP),
+    Card(Card.ZAP),
+    Card(Card.ZAP),
+    Card(Card.DEFRAGMENT),
+    Card(Card.DEFRAGMENT),
+    Card(Card.DEFRAGMENT),
+    Card(Card.DEFRAGMENT),
+    Card(Card.THUNDER_STRIKE),
+])
+
+defect_frost_deck = Deck([
+    Card(Card.GLACIER),
+    Card(Card.GLACIER),
+    Card(Card.GLACIER),
+    Card(Card.DEFRAGMENT),
+    Card(Card.DEFRAGMENT),
+    Card(Card.DEFRAGMENT),
+    Card(Card.BLIZZARD),
+    Card(Card.BLIZZARD),
+    Card(Card.BLIZZARD),
+    Card(Card.BLIZZARD),
+    Card(Card.BLIZZARD),
+])
