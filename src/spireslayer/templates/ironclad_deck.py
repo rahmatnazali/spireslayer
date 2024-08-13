019005d9fe19b8eab6145bc9286e71fcdbb40508
gabrekt <gabrielretamalesm@gmail.com>
2024-08-13 15:51:22 -0400

    added red and green card

---
+++ src/spireslayer/templates/ironclad_deck.py
@@ -0,0 +1,38 @@
+from spireslayer.card import Card
+from spireslayer.decks import Deck
+from spireslayer.templates.ironclad_card import *
+from spireslayer.templates.colorless_card import INSIGHT, FLASH_OF_STEEL
+
+#Strenght focus deck
+
+strenght_focus = Deck([
+    Card(FLEX),
+    Card(FLEX),
+    Card(INFLAME),
+    Card(INFLAME),
+    Card(RUPTURE),
+    Card(SPOT_WEAKNESS),
+    Card(LIMIT_BREAK),
+    Card(LIMIT_BREAK),
+    Card(HEAVY_BLADE),
+    Card(HEAVY_BLADE),
+    Card(HEAVY_BLADE),
+    Card(HEAVY_BLADE)
+])
+
+bodyslam = Deck([
+    Card(BARRICADE),
+    Card(ENTRENCH),
+    Card(BODY_SLAM),
+    Card(BODY_SLAM),
+    Card(BODY_SLAM),
+    Card(BODY_SLAM),
+    Card(BODY_SLAM),
+    Card(SHRUG_IT_OFF),
+    Card(SHRUG_IT_OFF),
+    Card(SHRUG_IT_OFF),
+    Card(IMPERVIOUS),
+    Card(IMPERVIOUS),
+    Card(IMPERVIOUS),
+    Card(SHRUG_IT_OFF)
+])
---
+++ src/spireslayer/templates/silent_deck.py
@@ -0,0 +1,43 @@
+from spireslayer.card import Card
+from spireslayer.decks import Deck
+from spireslayer.templates.silent_card import *
+
+
+#Poison deck
+
+poison = Deck([
+    Card(BOUNCING_FLASK),
+    Card(BOUNCING_FLASK),
+    Card(BOUNCING_FLASK),
+    Card(CATALYST),
+    Card(CATALYST),
+    Card(NOXIOUS_FUMES),
+    Card(NOXIOUS_FUMES),
+    Card(NOXIOUS_FUMES),
+    Card(BACKFLIP),
+    Card(BACKFLIP),
+    Card(DODGE_AND_ROLL),
+    Card(DODGE_AND_ROLL),
+    Card(BLUR),
+    Card(BLUR),
+    Card(CALTROPS)
+])
+
+#Shiv
+
+
+shiv_and_draw = Deck([
+    Card(ACCURACY),
+    Card(ACCURACY),
+    Card(ACCURACY),
+    Card(INFINITE_BLADES),
+    Card(INFINITE_BLADES),
+    Card(BLADE_DANCE),
+    Card(BLADE_DANCE),
+    Card(BLADE_DANCE),
+    Card(CLOAK_AND_DAGGER),
+    Card(CLOAK_AND_DAGGER),
+    Card(CLOAK_AND_DAGGER),
+    Card(PREPARED),
+    Card(PREPARED)
+])
